89c7dd50337bd158d75c591d835d518388774051
Wesley Lether <w.lether@lethermade.nl>
2023-12-09 08:58:52 +0100

    Added 2023 04 - part two

--- src/solutions/2023/04.rs
+++ src/solutions/2023/04.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 use regex::Regex;
 
 use advent_of_code::utils::challenges::prelude::*;
@@ -7,9 +9,9 @@ lazy_static! {
 	static ref NUMBERS:Regex = Regex::new(r"\d+").unwrap();
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct ScratchCard {
-	game: u32,
+	game: usize,
 	winning_numbers: Vec<u32>,
 	numbers: Vec<u32>,
 }
@@ -31,14 +33,18 @@ impl ScratchCard {
 			.collect()
 	}
 
+	fn matching_numbers_count(&self) -> u32 {
+		self.matching_numbers().iter().count() as u32
+	}
+
 	fn matching_numbers_worth(&self) -> u32 {
 		let mut result = 1;
-		let count = self.matching_numbers().iter().count();
+		let count = self.matching_numbers_count();
 		if count == 0 {
 			return 0;
 		}
 
-		for _ in 0..self.matching_numbers().iter().count() - 1 {
+		for _ in 0..count - 1 {
 			result *= 2;
 		}
 		result
@@ -52,7 +58,7 @@ fn parse(input: &PuzzleInput) -> Vec<ScratchCard> {
 		.enumerate()
 		.map(|(game, line)| {
 			let mut scratch_card = ScratchCard::new();
-			scratch_card.game = (game + 1) as u32;
+			scratch_card.game = game + 1;
 
 			let numbers_of_card: &str = line.split(':').collect::<Vec<&str>>().get(1).unwrap();
 			let numbers_split: Vec<&str> = numbers_of_card.split('|').collect::<Vec<&str>>();
@@ -84,7 +90,23 @@ fn part_one(input: &PuzzleInput, _args: &RawPuzzleArgs) -> Solution {
 }
 
 fn part_two(input: &PuzzleInput, _args: &RawPuzzleArgs) -> Solution {
-	Unsolved
+	let scratched_cards = parse(input);
+	let mut total = 0;
+	let mut copies: VecDeque<ScratchCard> = VecDeque::new();
+	copies.extend(scratched_cards.clone());
+
+	while let Some(sc) = copies.pop_front() {
+		total += 1;
+
+		let winning_numbers_count = sc.matching_numbers_count() as usize;
+		if winning_numbers_count > 0 {
+			for to_copy in &scratched_cards[sc.game..(sc.game + winning_numbers_count)] {
+				copies.push_back(to_copy.clone())
+			}
+		}
+	}
+
+	Answer(total)
 }
 
 solve!(part_one, part_two);
